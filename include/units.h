3480b196fbe7f6b2c3591bd2314394c4d22c383d
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-08-10 18:57:40 -0400

    Split charge unit definitions into units/charge.h

--- include/units.h
+++ include/units.h
@@ -50,6 +50,7 @@
 #include <units/acceleration.h>
 #include <units/angle.h>
 #include <units/angular_velocity.h>
+#include <units/charge.h>
 #include <units/current.h>
 #include <units/force.h>
 #include <units/frequency.h>
@@ -66,25 +67,6 @@
 namespace units
 {
 	// clang-format off
-
-	//------------------------------
-	//	UNITS OF CHARGE
-	//------------------------------
-
-	/**
-	 * @namespace	units::charge
-	 * @brief		namespace for unit types and containers representing charge values
-	 * @details		The SI unit for charge is `coulombs`, and the corresponding `dimension` dimension is
-	 *				`charge_unit`.
-	 * @anchor		chargeContainers
-	 * @sa			See unit for more information on unit type containers.
-	 */
-#if !defined(DISABLE_PREDEFINED_UNITS) || defined(ENABLE_PREDEFINED_CHARGE_UNITS)
-	UNIT_ADD_WITH_METRIC_PREFIXES(charge, coulomb, coulombs, C, unit_conversion<std::ratio<1>, units::dimension::charge>)
-	UNIT_ADD_WITH_METRIC_PREFIXES(charge, ampere_hour, ampere_hours, Ah, compound_unit_conversion<current::ampere, time::hours>)
-
-	UNIT_ADD_DIMENSION_TRAIT(charge)
-#endif
 
 	//------------------------------
 	//	UNITS OF ENERGY
