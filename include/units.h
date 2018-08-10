66dac1f6379a423deb75b87c4ec2efb45bebfed0
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-08-10 19:06:19 -0400

    Split conductance unit definitions into units/conductance.h

--- include/units.h
+++ include/units.h
@@ -52,6 +52,7 @@
 #include <units/angular_velocity.h>
 #include <units/capacitance.h>
 #include <units/charge.h>
+#include <units/conductance.h>
 #include <units/current.h>
 #include <units/energy.h>
 #include <units/force.h>
@@ -72,24 +73,6 @@
 namespace units
 {
 	// clang-format off
-
-	//------------------------------
-	//	UNITS OF CONDUCTANCE
-	//------------------------------
-
-	/**
-	 * @namespace	units::conductance
-	 * @brief		namespace for unit types and containers representing conductance values
-	 * @details		The SI unit for conductance is `siemens`, and the corresponding `dimension` dimension is
-	 *				`conductance_unit`.
-	 * @anchor		conductanceContainers
-	 * @sa			See unit for more information on unit type containers.
-	 */
-#if !defined(DISABLE_PREDEFINED_UNITS) || defined(ENABLE_PREDEFINED_CONDUCTANCE_UNITS)
-	UNIT_ADD_WITH_METRIC_PREFIXES(conductance, siemens, siemens, S, unit_conversion<std::ratio<1>, units::dimension::conductance>)
-
-	UNIT_ADD_DIMENSION_TRAIT(conductance)
-#endif
 
 	//------------------------------
 	//	UNITS OF MAGNETIC FLUX
