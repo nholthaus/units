dfa959c80fa90ae90582205b29519b2b292a946e
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-11-03 13:22:15 -0400

    Test the common type of dimensionless and arithmetic types

--- unitTests/main.cpp
+++ unitTests/main.cpp
@@ -901,6 +901,18 @@ TEST_F(STDTypeTraits, std_common_type)
 	static_assert(std::is_same_v<
 		std::common_type_t<traits::unit_base_t<dimensionless<int>>, traits::unit_base_t<dimensionless<double>>>,
 		traits::unit_base_t<dimensionless<double>>>);
+
+	static_assert(std::is_same_v<std::common_type_t<dimensionless<int>, int>, dimensionless<int>>);
+	static_assert(std::is_same_v<std::common_type_t<dimensionless<int>, double>, double>);
+	static_assert(std::is_same_v<std::common_type_t<dimensionless<double>, int>, dimensionless<double>>);
+	static_assert(std::is_same_v<std::common_type_t<dimensionless<double>, double>, dimensionless<double>>);
+	static_assert(std::is_same_v<std::common_type_t<traits::unit_base_t<dimensionless<int>>, int>,
+		traits::unit_base_t<dimensionless<int>>>);
+	static_assert(std::is_same_v<std::common_type_t<traits::unit_base_t<dimensionless<int>>, double>, double>);
+	static_assert(std::is_same_v<std::common_type_t<traits::unit_base_t<dimensionless<double>>, int>,
+		traits::unit_base_t<dimensionless<double>>>);
+	static_assert(std::is_same_v<std::common_type_t<traits::unit_base_t<dimensionless<double>>, double>,
+		traits::unit_base_t<dimensionless<double>>>);
 }
 
 TEST_F(STDSpecializations, hash)
